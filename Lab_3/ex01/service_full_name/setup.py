5636b158d99118e964cdb2617e669cd2d76b8d0e
Andrey <114009968+kvasik3000@users.noreply.github.com>
2023-10-04 00:27:11 +0700

    add lab3

---
+++ Lab_3/ex01/service_full_name/setup.py
@@ -0,0 +1,25 @@
+from setuptools import find_packages, setup
+
+package_name = 'service_full_name'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='kvasik',
+    maintainer_email='superadrenoline3000@gmail.com',
+    description='Python client server',
+    license='Apache License 2.0',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
