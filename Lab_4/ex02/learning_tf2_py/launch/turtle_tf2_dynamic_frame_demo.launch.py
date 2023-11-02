730e452370f62eb10e25bf7339b07032e365afe4
Andrey <114009968+kvasik3000@users.noreply.github.com>
2023-11-03 02:35:23 +0700

    Update turtle_tf2_dynamic_frame_demo.launch.py

--- Lab_4/ex02/learning_tf2_py/launch/turtle_tf2_dynamic_frame_demo.launch.py
+++ Lab_4/ex02/learning_tf2_py/launch/turtle_tf2_dynamic_frame_demo.launch.py
@@ -4,16 +4,15 @@ from ament_index_python.packages import get_package_share_directory
 
 from launch.actions import DeclareLaunchArgument
 from launch import LaunchDescription
+from launch.substitutions import LaunchConfiguration
 from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-
+from launch_ros.actions import SetParameter
 from launch_ros.actions import Node
 
 
 def generate_launch_description():
-    radius_arg = DeclareLaunchArgument('radius', default_value='10.0', description='Radius of rotation')
-    direction_arg = DeclareLaunchArgument('direction_of_rotation', default_value='1', description='Direction of rotation (1 - clockwise, -1 - counterclockwise)')
-
+    
     demo_nodes = IncludeLaunchDescription(
         PythonLaunchDescriptionSource([os.path.join(
             get_package_share_directory('learning_tf2_py'), 'launch'),
@@ -22,12 +21,20 @@ def generate_launch_description():
         )
 
     return LaunchDescription([
-        radius_arg,
-        direction_arg,
+        
+        SetParameter(
+            name='radius',
+            value=3.0  
+        ),
+        SetParameter(
+            name='direction_of_rotation',
+            value=-1  
+        ),
         demo_nodes,
         Node(
             package='learning_tf2_py',
-            executable='fixed_frame_tf2_broadcaster',
-            name='fixed_broadcaster',
+            executable='dynamic_frame_tf2_broadcaster',
+            name='dynamic_broadcaster',
+            
         ),
     ])
