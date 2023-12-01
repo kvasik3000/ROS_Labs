11019b9890b5933c514b880d0b68287a33bc4ca0
Andrey <114009968+kvasik3000@users.noreply.github.com>
2023-12-02 05:31:28 +0700

    add ex05
    half

---
+++ Lab_5/ex05/robot_walking/setup.py
@@ -0,0 +1,27 @@
+from setuptools import find_packages, setup
+
+package_name = 'robot_walking'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='kvasik',
+    maintainer_email='superadrenoline3000@gmail.com',
+    description='TODO: Package description',
+    license='Apache 2.0',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        	'robot_walking = robot_walking.robot_walking:main',
+        	'unique_robot_walking = robot_walking.unique_robot_walking:main',
+        ],
+    },
+)
